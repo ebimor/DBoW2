3c99e5bf71d570581a0706302938cec062b37eb5
Ebrahim <eshahrivar@clearpath.ai>
2020-07-06 20:07:49 -0400

    kmean cluster

--- demo/place_recog.cpp
+++ demo/place_recog.cpp
@@ -13,13 +13,18 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/features2d.hpp>
+#include <Eigen/Dense>
+#include <ctime> 
 
+using namespace Eigen;
 using namespace std;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 // number of training images
 const int NIMAGES = 4;
+
+typedef Matrix<int, 1, 32> MatrixOrbf;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -29,25 +34,26 @@ void wait()
   getchar();
 }
 
-void loadFeatures(vector<vector<cv::Mat > > &features);
+void loadFeatures(vector<cv::Mat > &features);
 void showFeatures(const vector<cv::KeyPoint>& keypoints, const cv::Mat& image);
 void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out);
-
+void  kmeanClusterFeatures(const vector<cv::Mat> &features, int k = 5);
+double calculateDistance(const cv::Mat& f1, const cv::Mat& f2);
 
 
 // ----------------------------------------------------------------------------
 
 int main()
 {
-  vector<vector<cv::Mat > > features;
-  loadFeatures(features);
+  vector<cv::Mat > featuresDB;
+  loadFeatures(featuresDB);
 
   return 0;
 }
 
 // ----------------------------------------------------------------------------
 
-void loadFeatures(vector<vector<cv::Mat > > &features)
+void loadFeatures(vector<cv::Mat > &features)
 {
   features.clear();
   features.reserve(NIMAGES);
@@ -67,11 +73,15 @@ void loadFeatures(vector<vector<cv::Mat > > &features)
 
     orb->detectAndCompute(image, mask, keypoints, descriptors);
 
-    showFeatures(keypoints, image);
+    //showFeatures(keypoints, image);
 
-    features.push_back(vector<cv::Mat >());
-    changeStructure(descriptors, features.back());
+    changeStructure(descriptors, features);
+
+    //std::cout<<"size of the descriptors is : "<<descriptors.size()<<std::endl;
   }
+
+  kmeanClusterFeatures(features);
+
 
 }
 
@@ -79,12 +89,14 @@ void loadFeatures(vector<vector<cv::Mat > > &features)
 
 void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out)
 {
-  out.resize(plain.rows);
 
   for(int i = 0; i < plain.rows; ++i)
   {
-    out[i] = plain.row(i);
+    out.push_back(plain.row(i));
+
   }
+  std::cout<<"added  "<<plain.rows<<" descriptors"<<std::endl;
+
 }
 
 //-----------------------------------------------------------------------------
@@ -98,4 +110,47 @@ void showFeatures(const vector<cv::KeyPoint>& keypoints, const cv::Mat& image)
     cv::imshow("Matches", keypoints_image);
     cv::waitKey(-1);
 
+}
+
+double calculateDistance(const cv::Mat& f1, const cv::Mat& f2)
+{
+
+/*
+	cv::Mat r = features[1]-features[0];
+	std::cout<<norm(features[1], features[0])<<std::endl;
+
+	double L =0;
+	for(int i=0;i<features[0].cols;++i){
+		int x = (int)features[1].at<uchar>(0,i)-(int)features[0].at<uchar>(0,i);
+		L += pow( x, 2.0);
+	}
+	std::cout<<sqrt(L)<<std::endl;
+*/
+
+	return norm(f1, f2);
+}
+
+void  kmeanClusterFeatures(const vector<cv::Mat> &features, int k)
+{
+ 	srand((unsigned)time(0)); 
+
+	vector<cv::Mat> clusterMean;
+	clusterMean.resize(k);
+	std::vector<int> random_indexes; 
+	random_indexes.resize(k);
+	for(int i=0; i<k; ){
+		int r = (rand()%features.size());
+    	auto it = std::find (random_indexes.begin(), random_indexes.end(), r);
+    	if(it == random_indexes.end()){
+    		i++;
+    		random_indexes.push_back(r);
+			clusterMean.push_back(features[r]);
+    	}
+	}	
+
+	for(auto it = features.begin(); it < features.end(); it++){
+		for(auto ij = it+1; ij < features.end(); ij++ ){
+
+		}
+	}
 }
